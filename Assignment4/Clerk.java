bc3e9aaa549f7f00444ac0c3be898b4bce8891c5
PurveshKrishnani <Purvesh.Krishnani@infoservices.com>
2024-07-02 01:09:01 +0530

    Assignment 4

--- Assignment4/Clerk.java
+++ Assignment4/Clerk.java
@@ -2,9 +2,20 @@ package Assignment4;
 
 public class Clerk   extends Employee{
 
+    public Clerk(String name, double salary, Integer employeeId, int speed, int accuracy) {
+        super(name, salary, employeeId);
+        this.speed = speed;
+        this.accuracy = accuracy;
+        setSalary(salary);
+    }
+
+    public Clerk(int speed, int accuracy) {
+        this.speed = speed;
+        this.accuracy = accuracy;
+    }
+
     private  int speed;
     private  int  accuracy;
-
 
     public int getSpeed() {
         return speed;
@@ -14,35 +25,49 @@ public class Clerk   extends Employee{
         this.speed = speed;
     }
 
+    public void setAccuracy(int accuracy) {
+        this.accuracy = accuracy;
+    }
     public int getAccuracy() {
         return accuracy;
     }
 
-    public void setAccuracy(int accuracy) {
-        this.accuracy = accuracy;
-    }
-
-
-    public Clerk(String name, Integer employeeId, double salary, int speed, int accuracy) {
-        super(name, employeeId, salary);
-        this.speed = speed;
-        this.accuracy = accuracy;
-    }
-
-    public Clerk() {
-        this.speed = speed;
-        this.accuracy = accuracy;
-    }
 
 
     @Override
     public void setSalary(double salary) {
-
-        if(speed > 70 &&   accuracy > 80 )
-        {
-            double newsalary = salary +1000;
-            setSalary(newsalary);
+        if (speed >= 70 && accuracy >= 80) {
+            super.setSalary(salary + 1000);
+        } else {
+            super.setSalary(salary);
         }
+    }
+
+
+
+    public  static  void main(String args[])
+    {
+        Clerk clerk = new Clerk("purvesh" ,15000,1001,50,50);
+
+
+
+       System.out.println( clerk.getSalary());
+       clerk.setSpeed(90);
+       clerk.setAccuracy(90);
+        clerk.setSalary(clerk.getSalary());
+
+        System.out.println( clerk.getSalary());
+
+
+        clerk.setSpeed(80);
+        clerk.setAccuracy(80);
+        clerk.setSalary(clerk.getSalary());
+
+        System.out.println( clerk.getSalary());
+
 
     }
+
 }
+
+
--- Assignment4/Employee.java
+++ Assignment4/Employee.java
@@ -2,43 +2,17 @@ package Assignment4;
 
 public abstract class Employee {
 
-
-    private String name;
     private  Integer employeeId;
     private  double  salary;
+    private String name;
 
-    public abstract void setSalary(double salary);
-
-    private enum  ManagerType{;
-        private String HR;
-       private  String sales;
+    public Integer getEmployeeId() {
+        return employeeId;
     }
 
-    public Employee(String name, Integer employeeId, double salary) {
-        this.name = name;
+    public void setEmployeeId(Integer employeeId) {
         this.employeeId = employeeId;
-        this.salary = salary;
     }
-
-
-    public Employee() {
-        this.name = name;
-        this.employeeId = employeeId;
-        this.salary = salary;
-    }
-
-
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "name='" + name + '\'' +
-                ", employeeId=" + employeeId +
-                ", salary=" + salary +
-                '}';
-    }
-
-
 
     public String getName() {
         return name;
@@ -48,15 +22,29 @@ public abstract class Employee {
         this.name = name;
     }
 
-    public Integer getEmployeeId() {
-        return employeeId;
-    }
-
-
     public double getSalary() {
         return salary;
     }
 
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
 
+    public Employee(String name, double salary, Integer employeeId) {
+        this.name = name;
+        this.salary = salary;
+        this.employeeId = employeeId;
+    }
+
+    public Employee() {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    private enum  ManagerType{;
+        private String HR;
+       private  String sales;
+    }
 }
